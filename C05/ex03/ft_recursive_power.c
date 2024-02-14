27cd7288c9055ff75825537de7d3ab76b29cf95a
jose5556 <josefilmeneses@gmail.com>
2024-02-14 07:51:30 +0000

    ex03 done

---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
+
+int	main(void)
+{
+	printf("%d\n", ft_recursive_power(5, 5));
+}
